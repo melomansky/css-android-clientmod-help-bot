f9359b06772393835a36648d11c3dbb92152e6d5
melomansky <prosto.fasterr3@gmail.com>
2023-07-20 19:59:17 +0000

    added module welcome to the chat

--- cm_bot/handlers/user.py
+++ cm_bot/handlers/user.py
@@ -1,5 +1,9 @@
-from aiogram import Router
-from aiogram.filters import Command, CommandStart
+from aiogram import Router, Bot
+from aiogram.filters import (
+    Command, CommandStart, IS_MEMBER,
+    IS_NOT_MEMBER
+    )
+from aiogram.filters.chat_member_updated import ChatMemberUpdated, ChatMemberUpdatedFilter
 from aiogram.types import Message
 
 user_router = Router()
@@ -40,3 +44,16 @@ async def user_cn(message: Message):
     m += "если ещё не подписан."
 
     await message.reply(m)
+
+
+@user_router.chat_member(ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER))
+async def on_user_join(event: ChatMemberUpdated, bot: Bot):
+
+    m = "Привет, @{}\n\n".format(event.from_user.username)
+    m += "Я бот помощник для ClientMod!\n"
+    m += "Команды:\n"
+    m += "/cm - получить последнюю версию клиентмода.\n"
+    m += "/cn - получить последнюю версию движка для клиентмода.\n\n"
+    m += "ClientMod | CustomEngine by AndraMidoxXx"
+
+    await bot.send_message(event.chat.id, m)
